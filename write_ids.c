b4559ff073a5f58982e5f0040c4ff58157ff5bf2
Ian Yee <iayfnp@gmail.com>
2024-09-16 21:10:22 -0400

    Lol

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Pencil-World";
+    char STUDENT_ID[] = "973013486";
 
     return 0 ;
 }
